00b9d0a95630310774fe4bd32928aed1ab274c62
NguyenTu-sudo <156572241+NguyenTu-sudo@users.noreply.github.com>
2024-09-30 00:27:11 +0700

    Delete FirstDialog.java

--- FirstDialog.java
+++
@@ -1,9 +0,0 @@
-package lab;
-
-import javax.swing.JOptionPane;
-public class FirstDialog{
-    public static void main(String[] args){
-        JOptionPane.showMessageDialog(null, "HelloWorld! How are you?");
-        System.exit(0);
-    }
-}
