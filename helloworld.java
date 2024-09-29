f428d06ca01a8c353bcdfa949369a8b082918943
NguyenTu-sudo <156572241+NguyenTu-sudo@users.noreply.github.com>
2024-09-30 00:29:34 +0700

    Delete helloworld.java

--- helloworld.java
+++
@@ -1,9 +0,0 @@
-package lab;
-
-public class helloworld {
-
-    public static void main(String args[]){
-        System.out.println("Xin chao \n cac ban!");
-        System.out.println("Hello \t world");
-    }
-}
